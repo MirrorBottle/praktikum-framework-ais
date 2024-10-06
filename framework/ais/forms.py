e363bcaa431c92f28f1fae137c4db7439ec52074
MirrorBottle <setiawanbayu66152@gmail.com>
2024-10-07 00:31:03 +0800

    pertemuan 6

--- framework/ais/forms.py
+++ framework/ais/forms.py
@@ -1,6 +1,6 @@
 # ais/forms.py
 from django import forms
-from .models.students import Students
+from .models import Students
 
 class StudentForm(forms.ModelForm):
     class Meta:
--- framework/ais/views.py
+++ framework/ais/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages 
-from .models.students import Students
+from django.http import JsonResponse
+from django.db.models import Q 
+from .models import Students
 from .forms import StudentForm
-
 # Create your views here.
 def homepage(request):
   return render(request, 'homepage/index.html')
@@ -11,8 +12,18 @@ def about(request):
   return render(request, 'homepage/about.html')
 
 def student_index(request):
+    query = request.GET.get('q')
     students = Students.objects.all()
-    return render(request, 'student/index.html', {'students': students})
+    if query:
+        students = Students.objects.filter(
+            Q(name__icontains=query) |
+            Q(nim__icontains=query) |
+            Q(email__icontains=query) |
+            Q(phone_number__icontains=query)
+        )
+    else:
+        students = Students.objects.all()
+    return render(request, 'student/index.html', {'students': students, 'query': query})
 
 def student_create(request):
     if request.method == 'POST':
@@ -24,3 +35,21 @@ def student_create(request):
     else:
         form = StudentForm()
     return render(request, 'student/create.html', {'form': form})
+  
+def student_update(request, student_id):
+    student = get_object_or_404(Students, id=student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data mahasiswa berhasil diubah!')
+            return redirect('student_index')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'student/update.html', {'form': form, 'student': student})
+
+def student_delete(request, student_id):
+    student = get_object_or_404(Students, id=student_id)
+    student.delete()
+    messages.success(request, 'Data mahasiswa berhasil dihapus')
+    return JsonResponse({'success': True})
